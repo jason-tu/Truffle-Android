1066a92bef30c8410db2ba525dc7cf1018c0fc96
Taslim Dosunmu <tjdosunmu@gmail.com>
2018-11-04 08:24:15 -0500

    button for getting pic of face

--- client/src/main/java/io/synople/truffle/client/CustomerActivity.kt
+++ client/src/main/java/io/synople/truffle/client/CustomerActivity.kt
@@ -1,10 +1,13 @@
 package io.synople.truffle.client
 
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.support.v4.app.FragmentActivity
+import android.widget.Button
 import com.google.firebase.firestore.FirebaseFirestore
 import io.synople.truffle.common.model.Item
 import io.synople.truffle.common.model.User
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class CustomerActivity : FragmentActivity() {
 
@@ -13,12 +16,22 @@ class CustomerActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer)
+        val bScanFace = findViewById<Button>(R.id.bScanFace)
+        bScanFace?.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragmentFrame,
+                IDFaceFragment.newInstance(profile)
+            ).commit()
+        }
 
-        val docRef = FirebaseFirestore.getInstance().collection("users").document("T3ZwXPVjVseIZnHgr1Vw")
+        val docRef = FirebaseFirestore.getInstance().collection("users").document("absCSuaFEhxFhcpzoHvr")
         docRef.get().addOnSuccessListener { documentSnapshot ->
             profile = documentSnapshot.toObject<User>(User::class.java)!!
 
-            supportFragmentManager.beginTransaction().replace(R.id.fragmentFrame, ProfileFragment.newInstance(profile)).commit()
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragmentFrame,
+                ProfileFragment.newInstance(profile)
+            ).commit()
         }
         // absCSuaFEhxFhcpzoHvr
 
