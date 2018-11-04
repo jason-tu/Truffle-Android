c4ce6102b2dc5430a7024134fbc764dc499ce191
Taslim Dosunmu <taslim@Taslims-MacBook-Pro.local>
2018-11-03 22:54:28 -0400

    starting to create recyclerView stuff for customers

---
+++ vendor/src/main/java/io/synople/truffle/vendor/CustomersFragment.kt
@@ -0,0 +1,36 @@
+package io.synople.truffle.vendor
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CustomersFragment : Fragment() {
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        linearLayoutManager = LinearLayoutManager(context)
+        recyclerView.layoutManager = linearLayoutManager
+
+
+        return inflater.inflate(R.layout.fragment_items, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = CustomersFragment()
+    }
+}
