8f163d1d7bf19b73c04273916a3ff6d834eb9c76
Voldemort1979 <jpjajeh@gmail.com>
2018-11-04 01:07:43 -0400

    Inner class changes.

--- client/src/main/java/io/synople/truffle/client/MainActivity.java
+++ client/src/main/java/io/synople/truffle/client/MainActivity.java
@@ -1,11 +1,13 @@
 package io.synople.truffle.client;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import io.synople.truffle.common.model.Ticket;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,52 +16,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
     }
 
-//    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-//        private List<Ticket>;
-//
-//        // Provide a reference to the views for each data item
-//        // Complex data items may need more than one view per item, and
-//        // you provide access to all the views for a data item in a view holder
-//        public static class MyViewHolder extends RecyclerView.ViewHolder {
-//            // each data item is just a string in this case
-//            public TextView mTextView;
-//            public MyViewHolder(TextView v) {
-//                super(v);
-//                mTextView = v;
-//            }
-//        }
-//
-//        // Provide a suitable constructor (depends on the kind of dataset)
-//        public MyAdapter(String[] myDataset) {
-//            mDataset = myDataset;
-//        }
-//
-//        // Create new views (invoked by the layout manager)
-//        @Override
-//        public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
-//                                                         int viewType) {
-//            // create a new view
-//            TextView v = (TextView) LayoutInflater.from(parent.getContext())
-//                    .inflate(R.layout.my_text_view, parent, false);
-//        ...
-//            MyViewHolder vh = new MyViewHolder(v);
-//            return vh;
-//        }
-//
-//        // Replace the contents of a view (invoked by the layout manager)
-//        @Override
-//        public void onBindViewHolder(MyViewHolder holder, int position) {
-//            // - get element from your dataset at this position
-//            // - replace the contents of the view with that element
-//            holder.mTextView.setText(mDataset[position]);
-//
-//        }
-//
-//        // Return the size of your dataset (invoked by the layout manager)
-//        @Override
-//        public int getItemCount() {
-//            return mDataset.length;
-//        }
-//    }
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+        private List<Ticket> tickets;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView mTextView;
+            public MyViewHolder(TextView v) {
+                super(v);
+                mTextView = v;
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(List<Ticket> ticket) {
+            tickets = ticket;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                         int viewType) {
+            // create a new view
+            TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item_ticket, parent, false);
+
+            MyViewHolder vh = new MyViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.mTextView.setText(tickets.indexOf(position));
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return tickets.size();
+        }
+    }
 
 }
